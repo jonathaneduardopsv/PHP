cfc31b854493ce07fc28f2885c71debea3c759b1
Jonathan <https://gitlab.com/jonathaneduardo>
2023-03-29 17:55:25 -0300

    operadores

---
+++ Ex003/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        $num = 300;
+        echo "O valor da variável é $num";
+        //0x = hexadecimal
+        //0b = binario
+    ?>
+</body>
+</html>
